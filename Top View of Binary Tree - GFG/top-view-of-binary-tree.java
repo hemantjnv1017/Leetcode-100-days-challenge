16b584aeb6c2728517d97141388b192998758d15
hemantjnv1017 <135831279+hemantjnv1017@users.noreply.github.com>
2023-07-26 17:29:06 +0530

    Added solution - LeetHub

--- Top View of Binary Tree - GFG/top-view-of-binary-tree.java
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.java
@@ -128,59 +128,54 @@ class Solution
 {
     //Function to return a list of nodes visible from the top view 
     //from left to right in Binary Tree.
-    
-    static class Info{
-        Node node;
-        int hd;
-        
-        public Info(Node node, int hd){
-            this.hd = hd;
-            this.node = node;
-        }
-    }
+    public static class Pair {
+		int hd;
+		Node node;
+
+		Pair(int hd, Node node) {
+			this.hd = hd;
+			this.node = node;
+		}
+	}
+	
     static ArrayList<Integer> topView(Node root)
     {
         // add your code
-        ArrayList<Integer> ans = new ArrayList<>();
-        
-        HashMap<Integer,Node> map = new HashMap<>();
-        Queue<Info> q = new LinkedList<>();
-        
-        int min = 0, max = 0;
-        q.add(new Info(root,0));
-        q.add(null);
-        
-        while(!q.isEmpty()){
-            Info curr = q.remove();
-            if(curr==null){
-                if(q.isEmpty()){
-                    break;
-                }else{
-                    q.add(null);
-                }
-            }else{
-                if(!map.containsKey(curr.hd)){
-                    map.put(curr.hd,curr.node);
-                }
-                if(curr.node.left!=null){
-                    q.add(new Info(curr.node.left,curr.hd-1));
-                    min = Math.min(min,curr.hd-1);
-                }
-                
-                if(curr.node.right!=null){
-                    q.add(new Info(curr.node.right, curr.hd+1));
-                    max = Math.max(max,curr.hd+1);
-                }
-            }
-        }
-        
-        for(int i=min; i<=max; i++){
-            ans.add(map.get(i).data);
-        }
-        return ans;
-        
+        // create a treemap
+		TreeMap<Integer,Integer> map = new TreeMap<>();
+		// create a queue to implement levelorder traversal
+		Queue<Pair> que = new LinkedList<>();
+		// add src in queue
+		que.add(new Pair(0, root));
+
+		while(que.size() > 0) {
+			//remove
+			Pair curr = que.remove();
+
+			// update in treemap
+			if(map.containsKey(curr.hd) == false) {
+				map.put(curr.hd, curr.node.data);
+			}
+
+			//add nbrs
+			if(curr.node.left != null) {
+				que.add(new Pair(curr.hd - 1, curr.node.left));
+			}
+
+			if(curr.node.right != null) {
+				que.add(new Pair(curr.hd + 1, curr.node.right));
+			}
+		}
+
+		//create an arraylist
+		ArrayList<Integer> arr = new ArrayList<>();
+		// add values from treemap
+		for(int key : map.keySet()) {
+			arr.add(map.get(key));
+		}
+
+		//return
+		return arr;
         
     }
-    
-   
 }
